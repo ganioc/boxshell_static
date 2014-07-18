62eece7478ee89a051f385f50c05889abc74d484
ganioc <ganioc.yang@gmail.com>
2014-07-18 18:51:05 +0800

    begin to write register code at server side

--- js/bs_register.js
+++ js/bs_register.js
@@ -1,93 +1,143 @@
 // javascript for bs_register.html manipulation
 function mark_error(name){
-  $("#"+name).parent().addClass("has-error");
-  $("#"+name).next().addClass("glyphicon-remove");
-  $("#"+name).next().next().fadeIn();
-  
+    $("#"+name).parent().addClass("has-error");
+    $("#"+name).next().addClass("glyphicon-remove");
+    $("#"+name).next().next().fadeIn();
+    
 }
 function clear_error(name){
-  $("#"+name).parent().removeClass("has-error has-success");
-  $("#"+name).next().removeClass("glyphicon-remove glyphicon-ok");
-  $("#"+name).next().next().fadeOut();
-
+    $("#"+name).parent().removeClass("has-error has-success");
+    $("#"+name).next().removeClass("glyphicon-remove glyphicon-ok");
+    $("#"+name).next().next().fadeOut();
 
 }
 function mark_success(name){
-  $("#"+name).parent().addClass("has-success");
-  $("#"+name).next().addClass("glyphicon-ok");
-  $("#"+name).next().next().fadeOut();
+    $("#"+name).parent().addClass("has-success");
+    $("#"+name).next().addClass("glyphicon-ok");
+    $("#"+name).next().next().fadeOut();
 
 
 }
 function validate_pattern(name,pattern){
-  //var pattern = /^[A-Za-z]+[A-Za-z0-9-_]*$/;
-  if(name.search(pattern) === 0){
-    return true;
-  }
-  else{
-    return false;
-  }
+    //var pattern = /^[A-Za-z]+[A-Za-z0-9-_]*$/;
+    if(name.search(pattern) === 0){
+	return true;
+    }
+    else{
+	return false;
+    }
 }
 function validate_username(){
-  var name = $("#name_register").val();
-  return validate_pattern(name,/^[A-Za-z]+[A-Za-z0-9-_]*$/);
+    var name = $("#name_register").val();
+    if( name.length > 30){
+	return false;
+    }
+    else
+	return validate_pattern(name,/^[A-Za-z]+[A-Za-z0-9-_]*$/);
 }
+
 function validate_emailaddress(){
-  var address = $("#email_register").val();
-  return validate_pattern(address,/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    var address = $("#email_register").val();
+    if(address.length > 60){
+	return false;
+    }
+    else
+	return validate_pattern(address,/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 
 }
+
 function validate_password(){
-  var pwd = $("#pwd_register").val();
-  
+    var pwd = $("#pwd_register").val();
+    if(pwd.length <3 || pwd.length > 40){
+	return false;
+    }
+    else
+	return true;
 }
 function validate_password2(){
-  var pwd = $("#pwd2_register");
-  
+    var pwd2 = $("#pwd2_register").val();
+    var pwd1 = $("#pwd_register").val();
+    if(pwd2 !== pwd1){
+	return false;
+    }
+    else
+	return true;
 }
 
+
+function check_warning(name){
+    if( name === "checkbox_terms"){
+	return !$("#checkbox_terms").prop('checked');
+    }
+    //else
+    if( $("#"+name).next().next().css("display") === "block")
+	return true;
+    else
+	return false;
+}
+// this function is for submit button checking
 function validate(){
-  
-  console.log($("#name_register").val());
-  // check name大小写字母和下划线数字,长度小于29,无空格
-  if(validate_username() === false){
-    mark_error("name_register");
-  }
-  
-  console.log($("#email_register").val());
-  // check mail address
-
-  console.log($("#pwd_register").val());
-  // check password
-
-  console.log($("#pwd2_register").val());
-  // check if equal password
-
-  console.log($("#checkbox_terms").prop("checked"));
-
-  return false;
+    if(check_warning("name_register") === false &&
+      check_warning("email_register") === false &&
+      check_warning("pwd_register") === false &&
+      check_warning("pwd2_register") === false &&
+      check_warning("checkbox_terms") === false){
+	return true;
+    }
+    if(check_warning("checkbox_terms") === false){
+	// show the tooltip on submit button
+	$("#checkbox_terms").tooltip('enable');
+    }
+    return false;
 
 }
-
+function keyup_callback(){
+    var validate_input = {
+	'name_register':validate_username,
+	'email_register':validate_emailaddress,
+	'pwd_register':validate_password,
+	'pwd2_register':validate_password2
+    };
+    return function(e){
+	if(validate_input[e.target.id]()===true){
+	    if(check_warning(e.target.id) === true){
+		clear_error(e.target.id);
+		mark_success(e.target.id);
+	    }
+	    else 
+		mark_success(e.target.id);
+	}
+	else{
+	    if( check_warning(e.target.id) === false){
+		clear_error(e.target.id);
+		mark_error(e.target.id);
+	    }
+	    else
+		mark_error(e.target.id);
+	}
+    };
+}
 $(document).ready(function() {
     // console.log("begin bs_register function");
     // console.log(document.URL);
-  //check
-  $("#name_register").keyup(function(e){
-    console.log(e.target.id);
-    if(validate_username()===true){
-      clear_error("name_register");
-      mark_success("name_register");
-    }
-    else{
-      clear_error("name_register");
-      mark_error("name_register");
-    }
-  });
-  
+    //check
+    $("#name_register").keyup(keyup_callback() );
+    $("#email_register").keyup(keyup_callback());
+    $("#pwd_register").keyup(keyup_callback());
+    $("#pwd2_register").keyup(keyup_callback());
+
+    $("#checkbox_terms").change(function(e){
+	// if it's on , remove the tooltip on submit button
+	if($(this).prop('checked')){
+	    $("#submit_register").tooltip('disable');
+	}
+	else{
+	    $("#submit_register").tooltip('enable');
+	}
+	// else , add a tooltip on
+    });
 
 });
-
 
 
 
