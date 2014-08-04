773d6748656806bce3041ebe4a9a7081cdbb3aee
spike yang <ganioc.yang@gmail.com>
2014-08-04 23:04:05 +0800

    create ajax get function in command module

--- js/bs_account.js
+++ js/bs_account.js
@@ -3,6 +3,15 @@
 $(document).ready(function() {
     console.log("begin bs_account function");
     //console.log(document.URL);
+    $("#btn-edit-account").click(function(){
+	//bring out account edit form, using ajax
+	console.log("edit pressed");
+
+	$("#paragraph-content").empty();
+
+	//hide the button , wait form to bring it back
+
+    });
     
 });
 
