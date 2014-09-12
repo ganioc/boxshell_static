82dd254aa44454e2a884ecb41004bdb02b20f6c2
ganioc <ganioc.yang@gmail.com>
2014-09-12 18:20:50 +0800

    click toggle

--- js/sch.js
+++ js/sch.js
@@ -10,25 +10,83 @@ $(window).resize(function(){
     });
 });
 
-function Schematic(canvas_name, file_name){
-    this.canvas = canvas_name;
+function Commandbar(cmd_toggle_name){
+    this.toggle_name = cmd_toggle_name;
+
+}
+
+function Schematic(info){
+    this.canvas = info.canvas_name;
     this.DOM = $("#" + this.canvas);
     this.ctx = this.DOM.get(0).getContext("2d");
     // obj.width = obj.ctx.canvas.width;
     // obj.height = obj.ctx.canvas.height;
-    this.file = file_name || "";
+    this.file = info.file_name || "";
+    
+    this.gallery_height = info.gallery_height;
+    this.commandbar_height = info.commandbar_height;
+    this.gallery_class =  info.gallery_class;
+    this.commandbar_class = info.commandbar_class;
+    this.init(info);
 
-    this.init();
+    this.info = info;
 }
 
-Schematic.prototype.init = function(){
-    console.log(this.canvas + " init");
-};
+Schematic.prototype.init = function(info){
 
+    console.log(this.canvas + " init");
+    var that = this;
+
+    $("#" + info.cmd_toggle_gallery).click(function(e){
+	e.preventDefault();
+	console.log("toggle gallery");
+	var temp = $("#"+ info.cmd_toggle_gallery 
+		     + " span");
+	// if it's up then down
+	if( !that.gallery_on()){
+	    that.show_gallery();
+	    temp.removeClass(info.toggle_glyphicon_up);
+	    temp.addClass(info.toggle_glyphicon_down);
+	}// if it's down
+	else {
+	    that.hide_gallery();
+	    temp.removeClass(info.toggle_glyphicon_down);
+	    temp.addClass(info.toggle_glyphicon_up);
+	}
+    });
+};
+Schematic.prototype.hide_gallery = function(){
+    // slide gallery down
+    $("." + this.gallery_class).animate({top:window.innerHeight});
+    // move commandbar down
+    $("." + this.commandbar_class).animate({top:window.innerHeight - this.commandbar_height});
+};
+Schematic.prototype.show_gallery = function(){
+    // slide gallery up
+    $("." + this.gallery_class).animate({top:(window.innerHeight - this.gallery_height)});
+    // move commandbar up
+        $("." + this.commandbar_class).animate({top:window.innerHeight - this.commandbar_height - this.gallery_height});
+};
+Schematic.prototype.gallery_on = function(){
+    var temp = $("#"+ this.info.cmd_toggle_gallery 
+		     + " span");
+    if( temp.hasClass(this.info.toggle_glyphicon_up)){
+	return false;
+    }
+    else
+	return true;
+};
 Schematic.prototype.resize = function(w,h){
     this.DOM.attr("width", w);
     this.DOM.attr("height", h);
-    $("div .gallery").css("top", ( h - 100 ) + "px");
+    if( this.gallery_on()){
+	$("div .gallery").css("top", ( h - this.gallery_height ) + "px");
+	$("div .commandbar").css("top", ( h- this.gallery_height - this.commandbar_height) + "px");
+    }
+    else{
+	$("div .gallery").css("top", h + "px");
+	$("div .commandbar").css("top", ( h- this.commandbar_height) + "px");
+    }
 };
 Schematic.prototype.set_bg = function(color){
     this.DOM.css("background", color);
@@ -50,8 +108,20 @@ function Panel(){
 $(document).ready(function() {
     console.log("begin schematic function");
     //console.log(document.URL);
+
+    // hide the scrollbar on windows
+    $('body').css("overflow","hidden");
     
-    var sch1 = new Schematic("canvas1");
+    var sch1 = new Schematic({
+	canvas_name:"canvas1",
+	gallery_height:100,
+	commandbar_height:30,
+	gallery_class:"gallery",
+	commandbar_class:"commandbar",
+	cmd_toggle_gallery:"cmd_toggle_gallery",
+	toggle_glyphicon_up:"glyphicon glyphicon-chevron-up",
+	toggle_glyphicon_down:"glyphicon glyphicon-chevron-down"
+    });
     teapot.window_list.push(sch1);
     sch1.set_bg("white");
 
