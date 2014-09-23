62aae2e3192ea9f40820bbc7eb36dbba5309163d
ganioc <ganioc.yang@gmail.com>
2014-09-23 18:42:35 +0800

    new ball

--- js/newslib.js
+++ js/newslib.js
@@ -1,6 +1,8 @@
 // javascript for bs_sch.html manipulation
+"use strict";
 
 var teapot = teapot||{};
+var width,height;
 
 $(document).ready(function() {
     console.log("begin new lib function");
@@ -10,108 +12,87 @@ $(document).ready(function() {
     $('body').css("overflow","hidden");
 
     $(window).resize(function(){
+
+	width = window.innerWidth;
+	height = window.innerHeight;
+
 	$("#frame_block").css("width",window.innerWidth)
 	.css("height",window.innerHeight);
+
 	$("#svg_root").attr("width",window.innerWidth + "px")
 	.attr("height", window.innerHeight + "px");
 
     });
+
     $(window).resize();
-    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-    var data =[
-	{
-	    "State": "ND",
-	    "Total": 641481,
-	    "Under 5 Years": 0.065,
-	    "5 to 13 Years": 0.105,
-	    "14 to 17 Years": 0.053,
-	    "18 to 24 Years": 0.129,
-	    "16 Years and Over": 0.804,
-	    "18 Years and Over": 0.777,
-	    "15 to 44 Years": 0.410,
-	    "45 to 64 Years": 0.260,
-	    "65 Years and Over": 0.147,
-	    "85 Years and Over": 0.028
-	},
-	{
-	    "State": "ST",
-	    "Total": 641481,
-	    "Under 5 Years": 0.165,
-	    "5 to 13 Years": 0.1105,
-	    "14 to 17 Years": 0.053,
-	    "18 to 24 Years": 0.129,
-	    "16 Years and Over": 0.804,
-	    "18 Years and Over": 0.777,
-	    "15 to 44 Years": 0.410,
-	    "45 to 64 Years": 0.260,
-	    "65 Years and Over": 0.147,
-	    "85 Years and Over": 0.028
-	},
-	{
-	    "State": "WASH",
-	    "Total": 641481,
-	    "Under 5 Years": 0.05,
-	    "5 to 13 Years": 0.15,
-	    "14 to 17 Years": 0.053,
-	    "18 to 24 Years": 0.129,
-	    "16 Years and Over": 0.804,
-	    "18 Years and Over": 0.777,
-	    "15 to 44 Years": 0.410,
-	    "45 to 64 Years": 0.260,
-	    "65 Years and Over": 0.147,
-	    "85 Years and Over": 0.028
-	}
 
-    ];
-    var margin = {top: 20, right: 40, bottom: 10, left: 40},
-    width = 960,
-    height = 250 - margin.top - margin.bottom;
+    // 
+    var fill = d3.scale.category10();
 
-    var format = d3.format(".1%"),
-	states,
-	age;
-
-    var x= d3.scale.linear()
-    .range([0, width]);
-    var y= d3.scale.ordinal()
-    .rangeRoundBands([0,height], .1);
-
-    var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("top")
-    .tickSize(-height - margin.bottom)
-    .tickFormat(format);
+    var nodes = d3.range(100).map(function(i) {
+	return {index: i};
+    });
+    var force = d3.layout.force()
+    .nodes(nodes)
+    .size([width, height])
+    .on("tick", tick)
+    .start();
 
     var svg = d3.select("svg")
-    .attr("width" , width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .style("margin-left", -margin.left + "px")
-    .append("g")
-    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+	    .attr("width", width)
+	    .attr("height", height);
 
-    svg.append("g").attr("class","x axis");
-    svg.append("g").attr("class","y axis")
-    .append("line")
-    .attr("class", "domain")
-    .attr("y2", height);
-
-    var menu = d3.select("#menu select")
-    .on("change", change);
+    var node = svg.selectAll(".node")
+	    .data(nodes)
+	    .enter().append("circle")
+	    .attr("class", "node")
+	    .attr("cx", function(d) { return d.x; })
+	    .attr("cy", function(d) { return d.y; })
+	    .attr("r", 8)
+	    .style("fill", function(d, i) { return fill(i & 3); })
+	    .style("stroke", function(d, i) { return d3.rgb(fill(i & 3)).darker(2); })
+	    .call(force.drag)
+	    .on("mousedown", function() { d3.event.stopPropagation(); });
     
-    
-    
-    function change(){
+    svg.style("opacity", 1e-6)
+	.transition()
+	.duration(1000)
+	.style("opacity", 1);
 
+    d3.select("body")
+	.on("mousedown", mousedown);
 
+    function tick(e) {
+
+	// Push different nodes in different directions for clustering.
+	var k = 6 * e.alpha;
+	nodes.forEach(function(o, i) {
+	    o.y += i & 1 ? k : -k;
+	    o.x += i & 2 ? k : -k;
+	});
+
+	node.attr("cx", function(d) { return d.x; })
+	    .attr("cy", function(d) { return d.y; });
     }
-    
 
-    function type(d){
-	d.value = +d.value;
-	return d;
-    }
+    function mousedown() {
+	nodes.forEach(function(o, i) {
+	    o.x += (Math.random() - .5) * 40;
+	    o.y += (Math.random() - .5) * 40;
+	});
+	force.resume();
+    }   
 
 });
+
+
+
+
+
+
+
+
+
 
 
 
