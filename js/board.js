0573969f55e8b2ee60d3ff3e338c33cf5db87023
ganioc <ganioc.yang@gmail.com>
2014-10-29 18:15:00 +0800

    d3.js click effect

--- js/board.js
+++ js/board.js
@@ -115,13 +115,12 @@
 		svg_width,
 		svg_margin;
 
-	    
 	    svg_DOM = "#"+ opt.name;
 	    svg_width = opt.width;
 	    svg_height = opt.height;
 	    svg_margin = opt.margin || 20;
 
-	    console.log("stop here for a rest");
+	    console.log("into Svg _init()");
 
 	    var color_low_end = "#FE2E2E";
 	    var color_hi_end = "#AAAA39";
@@ -144,7 +143,7 @@
 
 	    console.log("can't find the DOM");
 
-	    var svg_root;
+	    var svg_root,orig_focus;
 	    var focus ,  // this is the original form root
 		nodes ,// root is initialized here
 		view, 
@@ -159,7 +158,8 @@
 		focus = d;
 
 		var transition = d3.transition()
-			.duration(d3.event.altKey?7500:750)
+			//.duration(d3.event.altKey?7500:750)
+		        .duration(750)
 			.tween("zoom", function(d){
 			    var i = d3.interpolateZoom(view,[focus.x,focus.y,focus.r*2 + svg_margin]);
 			    return function(t){
@@ -194,8 +194,11 @@
 		console.log("enter into _update_data");
 
 		svg_root = _.clone(data);
+		orig_focus = _.clone(data);
 		focus = svg_root;  // this is the original form root
 		nodes = pack.nodes(svg_root);// root is initialized here
+		console.log("read nodes");
+		console.log(nodes);
 	
 		circle = svg.selectAll("circle")
 		    .data(nodes)
@@ -209,8 +212,22 @@
 		    })
 		    .style("stroke","black")
 		    .on("click",function(d){
+
 			if(focus !== d)
 			    _zoom(d);
+			d3.event.stopPropagation();
+		    })
+		    .on("mouseover", function(d){
+			var nodeSelection = d3.select(this).style({opacity:"0.5"});
+			//nodeSelection.select("text").style({opacity:"1.0"});
+
+		    })
+		    .on("mouseout", function(d){
+			var nodeSelection = d3.select(this).style({opacity:"1"});
+		    })
+		    .on("dblclick", function(d){
+			console.log("in svg dbl click");
+			console.log(d);
 			d3.event.stopPropagation();
 		    });
 
@@ -236,37 +253,148 @@
 		d3.select(self.frameElement).style("height", svg_height + "px");
 		console.log("out of update_data");
 	    };
-	    
+	    var _get_orig_data=function(){
+		return orig_focus;
+	    };
+	    var _get_object_from_name = function(name){
+		return _.find(nodes,function(c){
+		    return c.name === name;
+		});
+	    };
+
 	    return{
 		// interface for graph manipulation
-		update_data:_update_data
+		update_data:_update_data,
+		get_orig_data: _get_orig_data,
+		zoom_to_name:function(name){
+		    var d = _get_object_from_name(name);
+		    if(d){
+			console.log(d);
+			_zoom(d);
+		    }
+		
+		}
 	    };
 	};
+	    
 	return{
 	    init:function(opt,data){
 		if(!instance){
-
+		    console.log("no instance");
 		    instance = _init(opt);
 		    if(data){
 		    	instance.update_data(data);
 		    }
 		    else
 		    	console.log("no data in");
-		    console.log("no instance");
-		    
 		}
 		else{
 		    console.log("already init Svg");
 		    // initialize it
-		    
 		}
 	    },
 	    get_instance:function(){
+		console.log("try to retrieve isntance");
 		return instance;
 	    }
 	};
     }();
 
+    u.Util = function(){
+	var _is_object= function(ob){
+	    return typeof(ob) === "object";
+	};
+	// array is object
+	var _is_array = function(ob){
+	    return Object.prototype.toString.call(ob) === '[object Array]';
+	};
+	var _is_string = function(ob){
+	    return typeof(ob) === "string";
+	};
+	return{
+	    adjacent_keys:function(ob){
+		// if ob is an object
+		if(!_is_array(ob) && _is_object(ob)){
+		    return Object.keys(ob);
+		}
+	    },
+	    is_object:_is_object,
+	    is_array:_is_array,
+	    is_string:_is_string
+	};
+    }();
+    // typeahead for search input box
+    u.Symfind = function(){
+	var dataset = [],
+	    engine,
+	    box_name;
+	var dataset_add = function(name){
+	    dataset.push({val:name});
+	};
+	var dataset_clear = function(){ dataset = [];};
+	var dataset_update = function(node){
+	    var key_lst = u.Util.adjacent_keys(node);
+	    _.map(key_lst,function(d){
+		if( d === "name") 
+		    dataset_add(node[d]);
+		else if (d === "children"){
+		    _.map(node[d],function(c){
+			dataset_update(c);
+		    });
+		}
+	    });
+	};
+	var _get_engine = function(){return engine;};
+	var _init = function(input_name,data){
+	    console.log("into symfind init()");
+	    // get all names from data , store it in dataset
+	    //console.log(data);
+	    box_name = input_name;
+	    dataset_clear();
+	    dataset_update(data);
+	    console.log(dataset);
+
+	    engine = new Bloodhound({
+		name:"symbols",
+		local:dataset,
+		//remote:null,
+		datumTokenizer:
+		Bloodhound.tokenizers.obj.whitespace("val"),
+		queryTokenizer: 
+		Bloodhound.tokenizers.whitespace
+	    });
+	    var promise = engine.initialize();
+	    promise
+	    .done(function(){console.log("success bloodhound");
+			     console.log(engine.ttAdapter());
+			    })
+	    .fail(function(){console.log("fail bloodhound");
+			    });
+
+	    $("#" + input_name).typeahead(
+		{
+		    hint:true,
+		    minLength:1,
+		    highlight:true
+		},
+		{
+		    displayKey:"val",
+		    source:u.Symfind.get_engine().ttAdapter()
+		}
+	    );
+	    console.log("end of symfind init");
+	};
+	var _clear = function(){};
+	var _get_content = function(){
+	    return $("#" + box_name).typeahead("val");
+	};
+	return{
+	    init:_init,
+	    clear:_clear,
+	    get_engine:_get_engine,
+	    get_content:_get_content
+	};
+    }();
 })(this);// actually "this" means window
 
 var data_root = {
@@ -315,8 +443,8 @@ $(document).ready(function() {
 
     //init the sections
     u.Controller.init("pages");
-    //$("#sym-search-txt").typeahead();
-
+    
+    
     u.Ctl.set_btn("sch-cmd-setting",{
 	click:function(){
 	    console.log("setting clicked");
@@ -326,10 +454,7 @@ $(document).ready(function() {
 	click:function(){
 	    console.log("symbol clicked");
 	    u.Controller.show("/sch/lib/");
-	    u.Svg.init({
-	    name:"sym-svg",
-	    width:700,
-	    height:700}, data_root);
+
 	    //u.Svg.get_instance().update_data();
 	}
     });
@@ -339,6 +464,29 @@ $(document).ready(function() {
 	    u.Controller.show("/lib/sch/");
 	}
     });
+
+    // init Svg display
+    u.Svg.init({
+	name:"sym-svg",
+	width:700,
+	height:700}, data_root);
+    // initialize typeahead content
+    u.Symfind.init("sym-search-txt",u.Svg.get_instance().get_orig_data());
+    
+    $("#sym-search-btn").click(
+	function(e){
+	    e.preventDefault();
+	    console.log("find clicked");
+	    console.log(u.Symfind.get_content());
+	    // need to update u.Svg graph
+	    var name = u.Symfind.get_content();
+	    if(name){
+		u.Svg.get_instance().zoom_to_name(name);
+	    }
+	}
+    );
+
+
 
     $(window).bind("hashchange", function(){
 	console.log(location.hash);
